ee1abc18e87c5c8927c0c6ca8b7356b59ac8ce1b
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-07-05 00:25:12 +0530

    added constants.py to the anarchyChess package

---
+++ anarchyChess/constants.py
@@ -0,0 +1,15 @@
+import pygame
+
+# constants for width, height of pygame board
+# number of rows and columns in the chessboard
+# size of each square in the chess board
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH // COLS
+
+# Colors (rgb)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
