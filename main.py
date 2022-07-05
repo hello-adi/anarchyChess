d8a48d25cf9fdd67262808de41d5ba12d76c7871
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-07-05 14:53:37 +0530

    board can be drawn by using the Board class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from anarchyChess.constants import WIDTH, HEIGHT
+from anarchyChess.board import Board
 
 FPS = 60
 WIN = pygame.display.set_mode(
@@ -10,7 +11,7 @@ pygame.display.set_caption("anarchyChess")
 
 def main():
     run = True
-
+    board = Board()
     clock = pygame.time.Clock()
 
     while run:
@@ -23,6 +24,10 @@ def main():
             # when mouse is clicked, check if a piece was selected, if we are moving
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
+
+        board.draw_square(WIN)
+        # to update after drawing after every loop iteration
+        pygame.display.update()
 
     pygame.quit()
 
