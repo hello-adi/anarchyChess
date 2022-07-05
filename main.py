39f12476522e4f9de21ba9ea07cb5b02dd514b73
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-07-05 11:47:15 +0530

    event loop for quit added to main.py

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import pygame
 from anarchyChess.constants import WIDTH, HEIGHT
 
-WIN = pygame.display.set_mode(WIDTH, HEIGHT)
+FPS = 60
+WIN = pygame.display.set_mode(
+    (WIDTH, HEIGHT)
+)  # width and height to be passed as a tuple
 pygame.display.set_caption("anarchyChess")
+
+
+def main():
+    run = True
+
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            # quitting the game by making run false
+            if event.type == pygame.QUIT:
+                run = False
+            # when mouse is clicked, check if a piece was selected, if we are moving
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quit()
+
+
+main()
