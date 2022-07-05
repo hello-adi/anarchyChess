78ab5de3e6d3fc012b9a48898e43bdfe627733e4
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-07-05 14:52:29 +0530

    added board.py to draw the board

---
+++ anarchyChess/board.py
@@ -0,0 +1,25 @@
+# board.py handles all the seperate pieces moving, deleting the pieces,
+# drawing certain pieces and so on.
+
+import pygame
+from .constants import BLACK, RED, ROWS, SQUARE_SIZE  # dot constant for relative import
+
+
+class Board:
+    def __init__(self) -> None:
+        # internal representaion in a  2D list
+        self.board = []
+        self.selected_piece = None
+        # number of pieces left for red and white
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+
+    def draw_square(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, ROWS, 2):
+                pygame.draw.rect(
+                    win,
+                    RED,
+                    (row * SQUARE_SIZE, col * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
+                )
