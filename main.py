968414ad0be8cd6a0ea035dc270f9d1fe185cda0
Aditya Lakshminarayanan <aditya.net09@gmail.com>
2022-07-26 11:50:20 +0530

    can get pos of selected chess piece

--- main.py
+++ main.py
@@ -20,14 +20,30 @@ pygame.display.set_caption("anarchyChess")
 def redraw_pygame_window():
     global WIN
     WIN.blit(board, (0, 0))
-    chess_board = Board(8, 8)
+    # chess_board = Board(8, 8)
     chess_board.draw(WIN)
     # pygame.draw.rect(WIN, RED, (0, 0, 592, 592), 3)
     pygame.display.update()
 
 
+def click(pos):
+    """
+    :return: pos (x, y) in range 0-7 0-7
+    """
+    x = pos[0]
+    y = pos[1]
+    if rect[1] < x < rect[0] + rect[2]:
+        if rect[1] < y < rect[1] + rect[3]:
+            xDiv = x - rect[0]
+            yDiv = y - rect[0]
+            i = int(xDiv / (rect[2] / 8))
+            j = int(yDiv / (rect[3] / 8))
+            return i, j
+
+
 def main():
     run = True
+    chess_board = Board(8, 8)
     clock = pygame.time.Clock()
 
     while run:
@@ -41,7 +57,9 @@ def main():
                 run = False
             # when mouse is clicked, check if a piece was selected, if we are moving
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                i, j = click(pos)
+            chess_board[j][i].isSelected = True
 
         # board.draw_square(WIN)
         # # to update after drawing after every loop iteration
